8c53bb6f70224c4109a8d1e8bb53db7cf5b76f3b
naufal-dean <naufal.dean@gmail.com>
2020-02-28 06:46:56 +0700

    Add home animation

--- frontend/src/containers/Home/index.js
+++ frontend/src/containers/Home/index.js
@@ -3,13 +3,15 @@ import React from "react";
 import "./style.css";
 import Construction from "../../images/construction.png";
 import Gear from "../../images/gear.png";
+import GearBack from "../../images/gear_back.png";
 
 const Home = props => {
   return (
     <div>
       <div>
         <img src={Construction} alt="Under construction" className="mainImg" />
-      <img src={Gear} alt="Gear" className="gearImg" />
+        <img src={GearBack} alt="Gear" className="gearBackImg" />
+        <img src={Gear} alt="Gear" className="gearImg" />
       </div>
     </div>
   );
